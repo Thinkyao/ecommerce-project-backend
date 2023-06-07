a44499a24c26ccc92619524424bee8b043722132
Thinkyao <lisiyao217@gmail.com>
2023-06-07 15:26:53 -0700

    disable http patch method for customers'info

--- src/main/java/com/sia/ecommerce/repos/ProductRepository.java
+++ src/main/java/com/sia/ecommerce/repos/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -11,6 +12,7 @@ import com.sia.ecommerce.entities.Product;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @Repository
+@RepositoryRestResource
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
 	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
